febfd7def52d94ca3e3bcbfa07d799f1177ed271
naruarjun <naruarjun@gmail.com>
2018-05-26 01:08:13 +0530

    Update anime_gan.py

--- anime_gan.py
+++ anime_gan.py
@@ -24,69 +24,32 @@ def lrelu(x, n, leak=0.2):
 
 
 def process_data():
-	# cur_dir = os.getcwd()
-	# file_dir = os.path.join(cur_dir,'data')
-	# images=[]
-	# for pic in os.listdir(file_dir):
-	# 	images.append(os.path.join(file_dir,pic))
-	# dataset = tf.convert_to_tensor(images,dtype=tf.string)
-	# images_queue = tf.train.slice_input_producer([dataset])
-	# data = tf.read_file(images_queue[0])
-	# image = tf.image.decode_jpeg(data,channels=CHANNEL)
-	# image = tf.image.random_flip_left_right(image)
-	# image = tf.image.random_brightness(image, max_delta=0.1)
-	# image = tf.image.random_contrast(image, lower=0.9, upper=1.1)
-	# size = [HEIGHT,WIDTH]
-	# image = tf.image.resize_images(image, size)
-	# image.set_shape([HEIGHT, WIDTH, CHANNEL])
-	# image = tf.cast(image, tf.float32)
-	# image = image / 255.0
-
-	# iamges_batch = tf.train.shuffle_batch(
-	#     [image], batch_size=BATCH_SIZE,
-	#     num_threads=4, capacity=200 + 3 * BATCH_SIZE,
-	#     min_after_dequeue=200)
-	# num_images = len(images)
-
-	# return iamges_batch, num_images
-	current_dir = os.getcwd()
-    # parent = os.path.dirname(current_dir)
-	pokemon_dir = os.path.join(current_dir, 'small_anime_data')
-	images = []
-	for each in os.listdir(pokemon_dir):
-	    images.append(os.path.join(pokemon_dir, each))
-	# print images
-	all_images = tf.convert_to_tensor(images, dtype=tf.string)
-
-	images_queue = tf.train.slice_input_producer(
-	    [all_images])
-
-	content = tf.read_file(images_queue[0])
-	image = tf.image.decode_jpeg(content, channels=CHANNEL)
-	# sess1 = tf.Session()
-	# print sess1.run(image)
+	cur_dir = os.getcwd()
+	file_dir = os.path.join(cur_dir,'data')
+	images=[]
+	for pic in os.listdir(file_dir):
+		images.append(os.path.join(file_dir,pic))
+	dataset = tf.convert_to_tensor(images,dtype=tf.string)
+	images_queue = tf.train.slice_input_producer([dataset])
+	data = tf.read_file(images_queue[0])
+	image = tf.image.decode_jpeg(data,channels=CHANNEL)
 	image = tf.image.random_flip_left_right(image)
 	image = tf.image.random_brightness(image, max_delta=0.1)
 	image = tf.image.random_contrast(image, lower=0.9, upper=1.1)
-	# noise = tf.Variable(tf.truncated_normal(shape = [HEIGHT,WIDTH,CHANNEL], dtype = tf.float32, stddev = 1e-3, name = 'noise'))
-	# print image.get_shape()
-	size = [HEIGHT, WIDTH]
+	size = [HEIGHT,WIDTH]
 	image = tf.image.resize_images(image, size)
 	image.set_shape([HEIGHT, WIDTH, CHANNEL])
-	# image = image + noise
-	# image = tf.transpose(image, perm=[2, 0, 1])
-	# print image.get_shape()
-
 	image = tf.cast(image, tf.float32)
 	image = image / 255.0
 
-	iamges_batch = tf.train.shuffle_batch(
-        [image], batch_size=BATCH_SIZE,
-        num_threads=4, capacity=200 + 3 * BATCH_SIZE,
-        min_after_dequeue=200)
+	images_batch = tf.train.shuffle_batch(
+	    [image], batch_size=BATCH_SIZE,
+	    num_threads=4, capacity=200 + 3 * BATCH_SIZE,
+	    min_after_dequeue=200)
 	num_images = len(images)
 
-	return iamges_batch, num_images
+	return images_batch, num_images
+	
 
 def generator(input,input_dim,is_train,reuse=False):
 	c1 ,c2, c3, c4, c5 = 512, 256, 128, 64, 32  # channel num
